2cf10ded3a3fa7ef1b6ecf5a52902419ac31a068
kishores <samir.kishore@cdk.com>
2017-09-14 17:56:49 +0530

    Added db changes

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://root:admin@ec2-54-209-24-115.compute-1.amazonaws.com:27017/admin',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
